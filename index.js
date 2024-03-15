4884479965ea6871735ed7253923db09d212ea9c
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-16 05:46:55 +0800

    JS: Declared variables with queryselectors for their respective ids

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputEl = document.querySelector("#input-el");
+const convertBtn = document.querySelector("#convert-btn");
+const resetBtn = document.querySelector("#reset-btn");
+const metreFoot = document.querySelector("#metre-foot-p");
+const literGallon = document.querySelector("#liter-gallon-p");
+const kiloPound = document.querySelector("#kilo-pound-p");
